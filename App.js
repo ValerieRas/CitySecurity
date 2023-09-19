c7115952162da8db80677859fee2cc050e2d3939
valerieRas <rasolovalerie@gmail.com>
2023-09-19 14:59:20 +0200

    added splash and welcom screen

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Entypo from '@expo/vector-icons/Entypo';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 import * as SplashScreen from 'expo-splash-screen';
 import * as Font from 'expo-font';
+import Accueil from './Pages/Accueil'; 
 
 // Keep the splash screen visible while we fetch resources
 SplashScreen.preventAutoHideAsync();
-
 
 export default function App() {
 
@@ -15,50 +15,29 @@ export default function App() {
   useEffect(() => {
     async function prepare() {
       try {
-        // Pre-load fonts, make any API calls you need to do here
-        await Font.loadAsync(Entypo.font);
-        // Artificially delay for two seconds to simulate a slow loading
-        // experience. Please remove this if you copy and paste the code!
+
         await new Promise(resolve => setTimeout(resolve, 3000));
+
       } catch (e) {
         console.warn(e);
       } finally {
         // Tell the application to render
         setAppIsReady(true);
+        SplashScreen.hideAsync(); // Hide the splash screen when app is ready
       }
     }
     prepare();
   }, []);
 
-  const onLayoutRootView = useCallback(async () => {
-    if (appIsReady) {
-      // This tells the splash screen to hide immediately! If we call this after
-      // `setAppIsReady`, then we may see a blank screen while the app is
-      // loading its initial state and rendering its first pixels. So instead,
-      // we hide the splash screen once we know the root view has already
-      // performed layout.
-      await SplashScreen.hideAsync();
-    }
-  }, [appIsReady]);
-
   if (!appIsReady) {
-    return null;
+    return null; // Keep showing the splash screen until appIsReady is true
   }
 
-
+  // When appIsReady is true, display the WelcomePage component
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <>
+      <Accueil />
       <StatusBar style="auto" />
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- Pages/Accueil.js
+++ Pages/Accueil.js
@@ -1,1 +1,43 @@
-import {Formulaire} from './Formulaire';
+import React from 'react';
+import { View, Text, StyleSheet, Image, Pressable } from 'react-native';
+
+import Button from '../Components/ButtonAccueil';
+
+const AppIcon = require('../assets/adaptive-icon.png');
+
+export default function WelcomePage() {
+    return (
+        <View style={styles.container}>
+            <View style={styles.imageContainer}>
+                <Image source={AppIcon} styles={styles.image}></Image>
+            </View>
+            <Button label="Alertez-Nous !"/>
+            <Text style={styles.text}>Welcome to Your App!</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'white',
+    },
+    text: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: 'black', // Example text color
+    },
+    imageContainer: {
+        flex: 1,
+        width:300,
+        height:300,
+    },
+    image: {
+        width: 300,
+        height: 300,
+    },
+});
+
+
